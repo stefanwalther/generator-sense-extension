79d67623488b38af38ddeffeb2db6377434efc0c
Stefan Walther <swr@qlik.com>
2015-08-09 11:50:37 +0200

    [code-doc] utils.getLicense

--- app/utils.js
+++ app/utils.js
@@ -3,18 +3,24 @@ var fs = require( 'fs' );
 
 module.exports = {
 
+	/**
+	 * Generate a license
+	 *
+	 * @param options {object} Prompt options
+	 * @returns {string} License text
+	 */
 	getLicense: function ( options ) {
 
 		var license_file = __dirname + '/licenses/' + options.extLic + '.txt';
 		var lic_content = fs.readFileSync( license_file, 'utf8' );
 
-		// Make replacements for year and fullname.
+		// Make replacements based on the prompts
 		var temp_result = lic_content
 			.replace( /\[year\]/g, options.publishingYear )
 			.replace( /\[fullname\]/g, options.authorName )
 			.replace( /\[project\]/g, options.extName );
 
-		// Prefix every line with a >
+		// Prefix every line with a > (markdown cite)
 		var lineArray = temp_result.split( '\n' );
 		var result = [];
 		for ( var i = 0; i < lineArray.length; i++ ) {
